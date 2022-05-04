9ab5863dc24ba3ba7b67c07dff8aff4e10f7f80e
José Corte <josecarlos-corte@hotmail.com>
2022-05-04 09:49:49 -0300

    setup styled-components

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Index.js</h1>
+    </main>
+  )
+}
