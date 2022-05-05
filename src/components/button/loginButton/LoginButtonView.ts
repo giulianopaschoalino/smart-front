205c187c5ff77db48569efd10399dd61fb39c988
José Corte <josecarlos-corte@hotmail.com>
2022-05-05 15:03:04 -0300

    add login button

---
+++ src/components/button/loginButton/LoginButtonView.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const LoginButtonView = styled.button`
+  width: 80%;
+  height: 95px;
+
+  border-radius: 8px;
+  background: linear-gradient(88.75deg, #254F7F 0.18%, #888888 99.28);
+
+  font-family: 'Nunito Sans';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 32px;
+  line-height: 44px;
+  text-align: center;
+  letter-spacing: 0.03em;
+
+  color: #FFFFFF;
+`
