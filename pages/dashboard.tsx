0920abc9d8ce0f4cb2d3253fd18cdaf753313e2a
José Corte <josecarlos-corte@hotmail.com>
2022-05-06 19:25:16 -0300

    :lipstick: modifing ui of dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -3,31 +3,25 @@ import React from 'react'
 import { DashboardView } from '../styles/layouts/Dashboard/DashboardView'
 import MapCard from '../src/components/mapCard/MapCard'
 import GraphCard from '../src/components/graph/graphCard/GraphCard'
-import Sidebar from '../src/components/sidebar/Sidebar'
 
 export default function Dashboard() {
   return (
-    <div style={{
-      'display': 'flex',
-      'flexDirection': 'row'
-    }}>
-      <Sidebar />
-      <DashboardView>
-        <h1>Visão Geral</h1>
-        <span>Bem Vindo a Smart Energia</span>
-        <section className="cardsSection" >
-          <MapCard title='R$/MWh' subtitle='abril / 22' statistic='' imgSource='/moneyIcon.svg' />
-          <MapCard title='SE/CO' subtitle='Sudeste' statistic='R$ 273,54' imgSource='/mapSample.svg' />
-          <MapCard title='SE/CO' subtitle='Sudeste' statistic='R$ 273,54' imgSource='/mapSample.svg' />
-          <MapCard title='SE/CO' subtitle='Sudeste' statistic='R$ 273,54' imgSource='/mapSample.svg' />
-          <MapCard title='SE/CO' subtitle='Sudeste' statistic='R$ 273,54' imgSource='/mapSample.svg' />
-        </section>
+    <DashboardView>
+      <h1>Visão Geral</h1>
+      <span>Bem Vindo a Smart Energia</span>
+      <section className="cardsSection" >
+        <MapCard title='R$/MWh' subtitle='abril / 22' statistic='' imgSource='/moneyIcon.svg' />
+        <MapCard title='SE/CO' subtitle='Sudeste' statistic='R$ 273,54' imgSource='/mapSample.svg' />
+        <MapCard title='SE/CO' subtitle='Sudeste' statistic='R$ 273,54' imgSource='/mapSample.svg' />
+        <MapCard title='SE/CO' subtitle='Sudeste' statistic='R$ 273,54' imgSource='/mapSample.svg' />
+        <MapCard title='SE/CO' subtitle='Sudeste' statistic='R$ 273,54' imgSource='/mapSample.svg' />
+      </section>
 
-        <section className='dashboard'>
-          <GraphCard title='Consumo' subtitle='Gráfico de Consumo' consumption={25} />
-          <GraphCard title='Consumo' subtitle='Gráfico de Consumo' />
-        </section>
-      </DashboardView>
-    </div>
+      <section className='dashboard'>
+        <GraphCard title='Consumo' subtitle='Gráfico de Consumo' consumption={25} />
+        <GraphCard title='Consumo' subtitle='Gráfico de Consumo' />
+        <GraphCard title='Consumo' subtitle='Gráfico de Consumo' className='footerGraph' />
+      </section>
+    </DashboardView>
   )
 }
