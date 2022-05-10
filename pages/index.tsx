0790f7973ad32e3f4115fbb675d14a1f8af59f87
Alex Santos <alex.santos@klupp.tech>
2022-05-10 15:18:39 -0300

    login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react'
+import {AiOutlineEyeInvisible, AiOutlineEye} from 'react-icons/ai';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router'
+
+
 
 
 import { LoginView} from  '../styles/layouts/login/LoginView';
@@ -9,24 +12,39 @@ import { truncateSync } from 'fs';
 
 interface HomeInterface {
   auth: any
+
 }
 
+
+
 export default function Home({ auth }: HomeInterface) {
+
+  const [state, setstate]=useState(false);
+   const toggleBtn = ()=> {
+      setstate(prevState => !prevState);
+   }
 
   const router = useRouter()
   const rota = router.pathname
 
   return (
     <LoginView auth={rota} >
-      <Image src='/assets/marca1.svg' width={600} height={700}/>
+      <Image src='/assets/marca1.svg' width={500} height={600}/>
       <section className="container">
         <h1>Bem-Vindo</h1>
-        <h2>Estratégias Inteligentes em Gestão de Energia</h2>
+        <h2>Estratégias Inteligentes em<br /> Gestão de Energia</h2>
         <input type="text" placeholder='Login'/>
-        <input type="text" placeholder='Senha'/>
+
+        <input className="password-field" type={state?"text":"password"} placeholder= "Senha" />
+       <button className='btnInput' onClick={toggleBtn}>
+         {state ? <AiOutlineEyeInvisible /> :
+           <AiOutlineEye />
+         }
+       </button>
+
         <span>Esqueceu a senha ?</span>
         <Link href='/dashboard' >
-          <button>ENTRAR</button>
+          <button className='button'>ENTRAR</button>
         </Link>
 
         <fieldset>
@@ -40,3 +58,5 @@ export default function Home({ auth }: HomeInterface) {
     </LoginView>
   )
 }
+
+
