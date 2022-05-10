ca49939a66e14011c7ba69c0411729321e19025f
Alex Santos <alex.santos@klupp.tech>
2022-05-10 15:29:34 -0300

    page login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,35 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import {AiOutlineEyeInvisible, AiOutlineEye} from 'react-icons/ai';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router'
+import { LoginView} from  '../styles/layouts/Login/LoginView';
 
 
-
-
-import { LoginView} from  '../styles/layouts/login/LoginView';
-import { truncateSync } from 'fs';
-
-interface HomeInterface {
-  auth: any
-
-}
-
-
-
-export default function Home({ auth }: HomeInterface) {
+export default function Home() {
 
   const [state, setstate]=useState(false);
-   const toggleBtn = ()=> {
-      setstate(prevState => !prevState);
-   }
-
+  const toggleBtn = ()=> {
+    setstate(prevState => !prevState);
+  }
   const router = useRouter()
   const rota = router.pathname
 
   return (
     <LoginView auth={rota} >
       <Image src='/assets/marca1.svg' width={500} height={600}/>
-      <section className="container">
-        <h1>Bem-Vindo</h1>
-        <h2>Estratégias Inteligentes em<br /> Gestão de Energia</h2>
-        <input type="text" placeholder='Login'/>
+        <section className="container">
+          <h1>Bem-Vindo</h1>
+          <h2>Estratégias Inteligentes em<br /> Gestão de Energia</h2>
 
-        <input className="password-field" type={state?"text":"password"} placeholder= "Senha" />
-       <button className='btnInput' onClick={toggleBtn}>
-         {state ? <AiOutlineEyeInvisible /> :
-           <AiOutlineEye />
-         }
-       </button>
-
-        <span>Esqueceu a senha ?</span>
+          <input type="text" placeholder='Login'/>
+          <input className="password-field" type={state?"text":"password"} placeholder= "Senha" />
+          <button className='btnInput' onClick={toggleBtn}>
+              {state ? <AiOutlineEyeInvisible /> :
+                <AiOutlineEye />
+              }
+          </button>
+          <span>Esqueceu a senha ?</span>
         <Link href='/dashboard' >
           <button className='button'>ENTRAR</button>
         </Link>
