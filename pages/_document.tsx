423d959c0a8e131627047283b724cdc26df9f90f
joseCorte-exe <josecorte-corte@hotmail.com>
2022-05-12 12:39:29 -0300

    :label: fix styled-components

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,30 +5,21 @@ import { ServerStyleSheet } from 'styled-components';
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
+  // static getInitialProps({ renderPage }) {
+  //   // Step 1: Create an instance of ServerStyleSheet
+  //   const sheet = new ServerStyleSheet();
 
-    try {
-      ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
-      });
+  //   // Step 2: Retrieve styles from components in the page
+  //   const page = renderPage((App) => (props) =>
+  //     sheet.collectStyles(<App {...props} />),
+  //   );
 
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-      ...initialProps,
-      styles: (
-        <>
-          {initialProps.styles}
-          {sheet.getStyleElement()}
-        </>
-      )
-      };
-      } finally {
-        sheet.seal();
-      }
-    }
+  //   // Step 3: Extract the styles as <style> tags
+  //   const styleTags = sheet.getStyleElement();
+
+  //   // Step 4: Pass styleTags as a prop
+  //   return { ...page, styleTags };
+  // }
 
     render() {
       return (
