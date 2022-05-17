2844ecb8e5a3b40745263cf470d18b3185cf79e6
Alex Santos <alex.santos@klupp.tech>
2022-05-17 12:47:46 -0300

    add data

--- src/services/economiaAcumulada.js
+++ src/services/economiaAcumulada.js
@@ -1,1 +1,5 @@
-export const data = []
+export const EconomiaAcumulada = {
+data : [872, 903, 938, 985, 1021, 1055, 1094, 1133, 1181, 1226, 1294],
+data1 : [1445, 1615],
+label: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'ago', 'set', 'out', 'nov', 'dez']
+}
