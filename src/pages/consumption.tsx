2b79448bf24dbab597263321f6efa56bbd979111
joseCorte-exe <josecorte-corte@hotmail.com>
2022-05-17 14:03:30 -0300

    fix consumption page chart

--- src/pages/consumption.tsx
+++ src/pages/consumption.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Chart from '../components/graph/Chart'
 import Header from '../components/header/Header'
 import PageTitle from '../components/pageTitle/PageTitle'
+import { dataEconomiaIndicador } from '../services/economiaIndicador'
 import { ConsumptionView } from '../styles/layouts/consumption/ConsumptionView'
 
 export default function Consumption() {
@@ -10,7 +11,7 @@ export default function Consumption() {
       <Header name='' />
       <PageTitle title='Consumo' subtitle='Análise de Consumo'/>
       <section>
-        <Chart title='Indicador de custo' />
+        <Chart title='Indicador de Custo' subtitle='(Valores em R$/MWh)' data1={dataEconomiaIndicador.data1} data2={dataEconomiaIndicador.data2} label={dataEconomiaIndicador.labels} />
       </section>
     </ConsumptionView>
   )
