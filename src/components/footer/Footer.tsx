56123037c02e06e480c0a4e0d4f56ba908a5886b
joseCorte-exe <josecorte-corte@hotmail.com>
2022-05-19 17:59:47 -0300

    add footer import fix

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-mixed-spaces-and-tabs */
 import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
@@ -6,15 +5,12 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
 
-import { FooterView } from './footerView'
+import { FooterView } from './FooterView'
 
 export default function Footer() {
   return (
     <FooterView>
-
-  	 			<p>© 2022 - Smart Energia </p>
-
-
+      <p>© 2022 - Smart Energia </p>
     </FooterView>
   );
 }
