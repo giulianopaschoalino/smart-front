624719bd481f4dc849180c4d121d0cfd939a4bc9
joseCorte-exe <josecorte-corte@hotmail.com>
2022-06-17 16:24:23 -0300

    integrate charts

--- src/pages/pld/index.tsx
+++ src/pages/pld/index.tsx
@@ -8,11 +8,11 @@ import { parseCookies } from 'nookies';
 import React, { useEffect, useState } from 'react'
 
 import BasicButton from '../../components/buttons/basicButton/BasicButton';
-import Chart from '../../components/graph/Chart';
 import { LineBarChart } from '../../components/graph/LineBarChart';
 import LineChart from '../../components/graph/LineChart';
 import Header from '../../components/header/Header'
 import PageTitle from '../../components/pageTitle/PageTitle';
+import { api } from '../../services/api';
 import { EconomiaAcumulada } from '../../services/economiaAcumulada';
 import { EvolucaoPld } from '../../services/evolucaoPld';
 import getAPIClient from '../../services/ssrApi';
@@ -29,18 +29,98 @@ export default function pld({tableData, graphByHourData, graphByMonthData}: pldI
   const router = useRouter()
   const { region } = router.query
 
+  const [date, setDate] = useState('');
+  const [select, setSelect] = useState('NORDESTE');
   const [page, setPage] = useState<string>('table')
-  const [age, setAge] = React.useState('');
+  const [day, setDay] = useState<string>('2')
+
+  const [dataByDay, setDataByDay] = useState([])
+
+  const [sul, setSul] = useState([])
+  const [norte, setNorte] = useState([])
+  const [sudeste, setSudeste] = useState([])
+  const [nordeste, setNordeste] = useState([])
 
   const handleChange = (event: SelectChangeEvent) => {
-    setAge(event.target.value);
+    setSelect(event.target.value);
+  };
+  const handleChangeDay = (event: SelectChangeEvent) => {
+    setDay(event.target.value);
   };
 
-  useEffect(() => {
-    console.log(page)
-  }, [page])
+  const label = ['1', '2', '3', '4', '5', '6', '7', '8', '8', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30']
 
-  function handleGreen(minimo, mi, ma, maximo) {
+  function getDataByDay() {
+    api.post('/pld/daily', {
+      "limit": 20,
+      "offset": 0,
+      "filters": [
+          {"type" : "=", "field" : "mes_ref", "value": `${day}/2022`, "row": true},
+          {"type" : "=", "field" : "pld.submercado", "value": select}
+        ],
+      "order": [{ "field": "day_calc", "direction": "asc" }]
+    }).then(res => {
+      setDataByDay(res.data.data)
+    }).catch(exception => console.log(exception))
+  }
+
+  function getDataByHour() {
+    api.post('/pld/schedule', {
+      "limit": 20,
+      "offset": 0,
+      "filters": [
+          {"type" : "=", "field" : "dia_num", "value": date, "row": true},
+          {"type" : "=", "field" : "submercado", "value": "SUL"}
+        ],
+      "order": [{ "field": "hour", "direction": "asc" }]
+    }).then(res => {
+      setSul(res.data.data)
+    }).catch(exception => console.log(exception))
+
+    api.post('/pld/schedule', {
+      "limit": 20,
+      "offset": 0,
+      "filters": [
+          {"type" : "=", "field" : "dia_num", "value": date, "row": true},
+          {"type" : "=", "field" : "submercado", "value": "SUDESTE"}
+        ],
+      "order": [{ "field": "hour", "direction": "asc" }]
+    }).then(res => {
+      setSudeste(res.data.data)
+    }).catch(exception => console.log(exception))
+
+    api.post('/pld/schedule', {
+      "limit": 20,
+      "offset": 0,
+      "filters": [
+          {"type" : "=", "field" : "dia_num", "value": date, "row": true},
+          {"type" : "=", "field" : "submercado", "value": "NORTE"}
+        ],
+      "order": [{ "field": "hour", "direction": "asc" }]
+    }).then(res => {
+      setNorte(res.data.data)
+    }).catch(exception => console.log(exception))
+
+    api.post('/pld/schedule', {
+      "limit": 20,
+      "offset": 0,
+      "filters": [
+          {"type" : "=", "field" : "dia_num", "value": date, "row": true},
+          {"type" : "=", "field" : "submercado", "value": "NORDESTE"}
+        ],
+      "order": [{ "field": "hour", "direction": "asc" }]
+    }).then(res => {
+      setNordeste(res.data.data)
+    }).catch(exception => console.log(exception))
+  }
+
+  useEffect(() => {
+    getDataByHour()
+    getDataByDay()
+    console.log(dataByDay)
+  }, [date, day, select])
+
+  function handleCellColor(minimo, mi, ma, maximo) {
     if (minimo - mi >= 100 && minimo - mi < 200) {
       return 'green'
     } else if ( mi*2 >= 200 && mi*2 < 250 ) {
@@ -131,23 +211,65 @@ export default function pld({tableData, graphByHourData, graphByMonthData}: pldI
           <section className='toolsbar'>
             <div className='select'>
               <Select
-                value={age}
+                value={select}
                 onChange={handleChange}
                 displayEmpty
                 sx={{
                   width: '100%'
                 }}
               >
-                <MenuItem value={0}>Norte</MenuItem>
-                <MenuItem value={10}>Nordeste</MenuItem>
-                <MenuItem value={20}>Sul</MenuItem>
-                <MenuItem value={30}>Sudeste</MenuItem>
+                <MenuItem value={'NORTE'}>Norte</MenuItem>
+                <MenuItem value={'NORDESTE'}>Nordeste</MenuItem>
+                <MenuItem value={'SUL'}>Sul</MenuItem>
+                <MenuItem value={'SUDESTE'}>Sudeste</MenuItem>
               </Select>
             </div>
-            <input type="date" data-date="" data-date-format="DD MMMM YYYY" value="2015-08-09"/>
+            <Select
+                value={day}
+                onChange={handleChangeDay}
+                displayEmpty
+                sx={{
+                  width: '100%'
+                }}
+              >
+                <MenuItem value={'01'}>01</MenuItem>
+                <MenuItem value={'02'}>02</MenuItem>
+                <MenuItem value={'03'}>03</MenuItem>
+                <MenuItem value={'04'}>04</MenuItem>
+                <MenuItem value={'05'}>05</MenuItem>
+                <MenuItem value={'06'}>06</MenuItem>
+                <MenuItem value={'07'}>07</MenuItem>
+                <MenuItem value={'08'}>08</MenuItem>
+                <MenuItem value={'09'}>09</MenuItem>
+                <MenuItem value={'10'}>10</MenuItem>
+                <MenuItem value={'11'}>11</MenuItem>
+                <MenuItem value={'12'}>12</MenuItem>
+                <MenuItem value={'13'}>13</MenuItem>
+                <MenuItem value={'14'}>14</MenuItem>
+                <MenuItem value={'15'}>15</MenuItem>
+                <MenuItem value={'16'}>16</MenuItem>
+                <MenuItem value={'17'}>17</MenuItem>
+                <MenuItem value={'18'}>18</MenuItem>
+                <MenuItem value={'19'}>19</MenuItem>
+                <MenuItem value={'20'}>20</MenuItem>
+                <MenuItem value={'21'}>21</MenuItem>
+                <MenuItem value={'22'}>22</MenuItem>
+                <MenuItem value={'23'}>23</MenuItem>
+                <MenuItem value={'24'}>24</MenuItem>
+                <MenuItem value={'25'}>25</MenuItem>
+                <MenuItem value={'26'}>26</MenuItem>
+                <MenuItem value={'27'}>27</MenuItem>
+                <MenuItem value={'28'}>28</MenuItem>
+                <MenuItem value={'29'}>29</MenuItem>
+                <MenuItem value={'30'}>30</MenuItem>
+              </Select>
             <BasicButton title='Download (csv)' onClick={() => console.log()}/>
           </section>
-          <LineBarChart data1={EvolucaoPld.data} data3={EvolucaoPld.data1} dataset1={'Economia'} dataset2={'barra1'} dataset3={'2021'} label={EvolucaoPld.label} title='Evolução PLD (R$/MWh)' subtitle='' />
+          <LineBarChart
+          data1={dataByDay} data3={dataByDay}
+          dataset1={'Economia'} dataset2={'barra1'} dataset3={'2021'}
+          label={EvolucaoPld.label}
+          title='Evolução PLD (R$/MWh)' subtitle='' />
         </PldGraphView>
       </RenderIf>
 
@@ -156,10 +278,13 @@ export default function pld({tableData, graphByHourData, graphByMonthData}: pldI
         <PldGraphView>
           <PageTitle title='Resumo PLD - Horas' subtitle=''/>
           <section className='toolsbar'>
-            <input type="date" data-date="" data-date-format="DD MMMM YYYY" value="2021-09-19"/>
+            <input type="date" data-date="" data-date-format="DD MMMM YYYY" value={date} onChange={(value) => setDate(value.target.value)}/>
             <BasicButton title='Download (csv)' onClick={() => console.log()}/>
           </section>
-          <LineChart data1={EconomiaAcumulada.data3} data2={EconomiaAcumulada.data4} data3={EconomiaAcumulada.data5} data4={EconomiaAcumulada.data6} dataset1='NORDESTE' dataset2='NORTE' dataset3='SUDESTE' dataset4='SUL' title='PLD - 19/09/21' subtitle='' label={EconomiaAcumulada.label1} />
+          <LineChart data1={nordeste} data2={norte} data3={sudeste} data4={sul}
+          dataset1='NORDESTE' dataset2='NORTE' dataset3='SUDESTE' dataset4='SUL'
+          title={`PLD - ${date}`}
+          subtitle='' label={['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24']} />
         </PldGraphView>
       </RenderIf>
     </main>
@@ -173,7 +298,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   let tableData = [];
 
   await apiClient.post('/pld/list').then(res => {
-    tableData = res.data
+    tableData = res.data.data
   }).catch(res => {
     console.log(res)
   })
