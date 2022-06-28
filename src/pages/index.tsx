2631df84f52ac22f1a6f6857c6e25aa685900be5
Alex Santos <alex.santos@klupp.tech>
2022-06-28 16:37:06 -0300

    Fixing update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -70,7 +70,7 @@ export default function Home() {
     }else{
       try {
         await signIn({email, password}).then((res: any) => {
-          if (res.response.status === 422 || res.response.status === 401) {
+          if (res.response.status === 422 || res.response.status === 401 || res.response.status === 500 ) {
             setOpenSnackError(true)
           }
         })
