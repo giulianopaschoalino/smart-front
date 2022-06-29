3084f746071d7382a75b96eab291f7accb299ab1
joseCorte-exe <josecorte-corte@hotmail.com>
2022-06-29 14:56:03 -0300

    try fix font

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  optimizeFonts: false,
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
   },
   images: {
     domains: ["kluppdevelopment.s3.sa-east-1.amazonaws.com"]
- }
+  }
 }
 
 module.exports = nextConfig
