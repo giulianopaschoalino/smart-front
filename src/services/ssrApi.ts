801ca929b2e1a13b28cabcf0a4426a87456cf378
joseCorte-exe <josecorte-corte@hotmail.com>
2022-08-03 15:07:16 -0300

    changing api address

--- src/services/ssrApi.ts
+++ src/services/ssrApi.ts
@@ -13,7 +13,7 @@ export default function getAPIClient(ctx?: Pick<next.NextPageContext, 'req'> | {
   const { '@smartAuth-token': token } = parseCookies(ctx)
 
   const api = axios.create({
-    baseURL: "https://smartenergiaapi.klupp.com.br/api",
+    baseURL: "https://api.energiasmart.com.br/api",
   });
 
   api.interceptors.request.use(config => {
